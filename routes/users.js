604600ddffad743f7d9b174ce86c13259cd21d44
Salvia Rahman <71405170+salvia-lab@users.noreply.github.com>
2021-09-28 13:40:02 -0400

    first commit

---
+++ routes/users.js
@@ -0,0 +1,10 @@
+//Imtiaj Hossain: student ID: 301119638
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('Place holder');
+});
+
+module.exports = router;
